f1af9a9f1b72125d531a74105191689d743bbc95
daniel21h <hesseldaniel21@gmail.com>
2020-07-05 12:04:39 -0300

    feat: calculating rule of 3 to update the bar

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
+// Get the text container
+const postWrap = document.querySelector('.post-wrap')
+
 // Create bar
 let bar = document.createElement('div')
 
 // Bar style
 bar.style.height = '5px'
-bar.style.width = '500px'
+bar.style.width = '0'
 bar.style.backgroundColor = '#6633cc'
 bar.style.position = 'fixed'
 bar.style.top = '0'
 bar.style.left = '0'
+bar.style.zIndex = '9999'
 
+// Add bar in html body
 document.body.append(bar)
+
+// Update bar
+
+// Check scroll movement
+document.addEventListener('scroll', () => {
+  // Size of the box containing the text
+  // console.log(postWrap.offsetHeight)
+  const textHeight = postWrap.offsetHeight
+
+  // Check which page position I am in
+  // console.log(window.pageYOffset)
+  const pagePositionY = window.pageYOffset
+
+  // Rule of 3
+  const updatedBar = textHeight * 100 / pagePositionY
+
+  console.log(updatedBar)
+})
