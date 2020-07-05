16b366df34eacb69c8f237f7c886b9b3cca7013a
daniel21h <hesseldaniel21@gmail.com>
2020-07-05 11:48:27 -0300

    fist commit and create bar

---
+++ script.js
@@ -0,0 +1,12 @@
+// Create bar
+let bar = document.createElement('div')
+
+// Bar style
+bar.style.height = '5px'
+bar.style.width = '500px'
+bar.style.backgroundColor = '#6633cc'
+bar.style.position = 'fixed'
+bar.style.top = '0'
+bar.style.left = '0'
+
+document.body.append(bar)
